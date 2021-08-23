4eee9358d4b19d47fd6b82a2ad2b26e647004de5
Emily Y <emilyyst19@hotmail.com>
2021-08-23 10:11:39 -0400

    Added tip icon
    - added tip icon + window shade for smaller devices
    - updated and moved github link

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const shadeScreenSize = 650;
+
 let cursor = new Cursor(cursorSet[0]);
 
 // set up cursor selection buttons
@@ -8,3 +10,17 @@ document.querySelectorAll("#cursor-selection-box button").forEach((btn, i) => {
 });
 
 setHoverListeners();
+
+// for tip
+let shade = document.getElementById("shade");
+
+document.getElementById("tip-icon").addEventListener("mouseenter", ev => {
+  if (window.innerHeight < shadeScreenSize) {
+    shade.classList.remove("active");
+    shade.classList.add("active");
+  }
+});
+
+document.getElementById("tip-icon").addEventListener("mouseleave", ev => {
+  shade.classList.remove("active");
+});
